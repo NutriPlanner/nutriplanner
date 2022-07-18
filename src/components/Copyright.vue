0360c0f91191237cbc261ef834689c3da3093d99
Jesus Barrientos <jesus.barrientosg.95@gmail.com>
2022-07-18 11:27:46 +0200

    feat: created login view

---
+++ src/components/Copyright.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="np-component np-component--copyright">
+    <div class="py-3">
+      <span class="np-component--copyright__text">
+        <small>Hecho por Jesús Barrientos con ❤️</small>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CopyrightComponent'
+}
+</script>
---
+++ src/pages/login.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="np-page np-page--login">
+    <div class="pt-5 pb-3">
+      <b-row align-h="center">
+        <b-col>
+          <b-card title="Inicio de Sesión" class="np-page--login__card">
+            <b-card-text>
+              <b-form @submit="onSubmit">
+                <b-form-group
+                  id="input-group-username"
+                  label="Correo electrónico"
+                  label-for="input-username"
+                >
+                  <b-form-input
+                    id="input-username"
+                    v-model="form.username"
+                    type="email"
+                    required
+                    trim
+                  />
+                </b-form-group>
+
+                <b-form-group
+                  id="input-group-password"
+                  label="Contraseña"
+                  label-for="input-password"
+                >
+                  <b-form-input
+                    id="input-password"
+                    v-model="form.password"
+                    type="password"
+                    required
+                  />
+                </b-form-group>
+              </b-form>
+            </b-card-text>
+
+            <b-button block type="submit" variant="primary">
+              Ingresar
+            </b-button>
+
+            <b-button block variant="link">
+              Recuperar contraseña
+            </b-button>
+          </b-card>
+        </b-col>
+      </b-row>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginPage',
+  layout: 'start',
+  data () {
+    return {
+      form: {
+        username: ''
+      }
+    }
+  },
+
+  methods: {
+    onSubmit (event) {
+      event.preventDefault()
+      this.$router.push('/')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.np-page--login__card {
+  margin: auto;
+  max-width: 25rem;
+}
+
+</style>
