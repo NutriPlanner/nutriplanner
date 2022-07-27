fd26dcd5c823087ca6f7a2c5f8579f28dc1587bf
Jesus Barrientos <jesus.barrientosg.95@gmail.com>
2022-07-27 22:17:33 +0200

    chore: added prettier formatter

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,18 +2,22 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   parserOptions: {
-    parser: '@babel/eslint-parser',
-    requireConfigFile: false
+    parser: "@babel/eslint-parser",
+    requireConfigFile: false,
   },
   extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended'
+    "@nuxtjs",
+    "plugin:nuxt/recommended",
+    "prettier",
+    "plugin:prettier/recommended",
   ],
-  plugins: [
-  ],
+  plugins: [],
   // add your custom rules here
-  rules: {}
-}
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+  },
+};
